27a8169121d780fd6cea1fc320a22106c41e1d5d
Philipp Hansch <dev@phansch.net>
2017-11-21 08:39:32 +0100

    Show lesson count
    Obtained by counting the files in the lessons directory

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 extern crate pancurses;
 
+use std::fs::read_dir;
 use pancurses::{initscr, endwin};
 
 fn main() {
     let window = initscr();
-    window.printw("Hello Rust");
+    let greeting = format!("The following {} lessons are available:", lesson_count());
+    window.printw(&greeting);
     window.refresh();
     window.getch();
     endwin();
+}
+
+fn lesson_count() -> usize {
+    // TODO: Handle errors correctly (files missing etc)
+    read_dir("lessons/").unwrap().count()
 }
