c43717f4bab07e47e312c7a30833997ab99e67bc
Philipp Hansch <dev@phansch.net>
2017-11-24 08:41:52 +0100

    Display contents of directory

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+// NEXT: List lessons in middle of screen
 extern crate pancurses;
 
-use std::fs::read_dir;
+use std::fs::{read_dir, DirEntry};
+use std::path::PathBuf;
 use pancurses::{initscr, endwin};
 
 fn main() {
     let window = initscr();
     let greeting = format!("The following {} lessons are available:", lesson_count());
+
     window.printw(&greeting);
+
+    for path in lesson_list() {
+        window.printw(&path.into_os_string().into_string().unwrap());
+    }
     window.refresh();
     window.getch();
     endwin();
@@ -15,4 +22,19 @@ fn main() {
 fn lesson_count() -> usize {
     // TODO: Handle errors correctly (files missing etc)
     read_dir("lessons/").unwrap().count()
+}
+
+// TODO:
+// Not sure if this is a good way to handle errors and still adhere to the function return type?
+// Are there better ways to do this?
+// Is walkdir overkill?
+fn lesson_list() -> Vec<std::path::PathBuf> {
+    let paths: Vec<std::path::PathBuf> = match read_dir("lessons/") {
+        Err(error) => {
+            println!("! {:?}", error.kind());
+            vec![PathBuf::new()]
+        },
+        Ok(paths) => paths.map(|path| path.unwrap().path()).collect(),
+    };
+    paths
 }
