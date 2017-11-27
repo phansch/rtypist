7e4f72fce146a0854c9e79e717500d484073501d
Philipp Hansch <dev@phansch.net>
2017-11-27 08:55:03 +0100

    Replace pancurses with Cursive
    Cursive is much much easier to use.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,41 @@
-// NEXT: List lessons in middle of screen
-extern crate pancurses;
+extern crate cursive;
 extern crate walkdir;
 
 use walkdir::{WalkDir};
-use pancurses::{initscr, endwin};
+use cursive::Cursive;
+use cursive::align::HAlign;
+use cursive::event::EventResult;
+use cursive::traits::*;
+use cursive::views::{Dialog, OnEventView, SelectView, TextView};
 
 fn main() {
-    let window = initscr();
+    let mut siv = Cursive::new();
+
+    let mut select = SelectView::new().h_align(HAlign::Center);
+
     let greeting = format!("The following {} lessons are available: ", lesson_count());
 
-    window.printw(&greeting);
+    let content = lesson_list();
+    select.add_all_str(content);
 
-    for path in lesson_list() {
-        window.printw(&path);
-        window.printw(" ");
-    }
-    window.refresh();
-    window.getch();
-    endwin();
+    // Behave roughly like ncurses with key presses
+    siv.add_global_callback('q', Cursive::quit);
+    let select = OnEventView::new(select)
+        .on_pre_event_inner('k', |s| {
+            s.select_up(1);
+            Some(EventResult::Consumed(None))
+        })
+        .on_pre_event_inner('j', |s| {
+            s.select_down(1);
+            Some(EventResult::Consumed(None))
+        });
+
+    siv.add_layer(
+        Dialog::around(select.fixed_size((20, 10)))
+        .title(greeting),
+    );
+
+    siv.run();
 }
 
 fn lesson_dir() -> WalkDir {
